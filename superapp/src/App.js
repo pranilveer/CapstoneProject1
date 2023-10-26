420762772eed27d08fe950bdc5ef97131d634cec
Pranil Dilip Veer <veerpranil@gmail.com>
2023-10-26 15:59:28 +0530

    added routing

--- superapp/src/App.js
+++ superapp/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { Routes, Route, Link} from "react-router-dom";
 import RegisterPage from './pages/RegisterPage';
+import GenrePage from './pages/GenrePage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route path="/register" element={<RegisterPage />}/>
+      <Route path="/genre" element={<GenrePage />}/>
       </Routes>
     </div>
   );
